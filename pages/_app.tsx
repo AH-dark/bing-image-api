934d967980214a6843b1dd6d0eb7c95b09398481
AH-dark <ahdark@outlook.com>
2022-03-16 20:58:34 +0800

    Feat: more concise and beautiful homepage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import type { AppProps } from "next/app";
+import type { AppProps }              from "next/app";
+import { createTheme, ThemeProvider } from "@mui/material";
+import "@fontsource/roboto"
+
+const theme = createTheme();
 
 // 新创建的 `pages/_app.js` 文件中必须有此默认的导出（export）函数
 export default function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
     Box,
     Container,
@@ -6,25 +6,39 @@ import {
     Divider,
     Link,
     Paper,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
     Theme,
     Typography,
 } from "@mui/material";
 import HeaderBar from "../components/HeaderBar";
 import Head from "next/head";
-import "@fontsource/roboto";
 import { createStyles, makeStyles } from "@mui/styles";
-import { useRouter } from "next/router";
-import { TableType } from "../types/config/table.config";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
-        root: {
+        background: {
+            backgroundImage: "url(/api/new)",
+            opacity: 1,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            display: "block",
+            position: "fixed",
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            zIndex: -1,
+        },
+        container: {
+            minHeight: "calc(100vh - 16px)",
+            display: "flex",
+            flexDirection: "column",
+            alignContent: "center",
+            justifyContent: "center",
+        },
+        paper: {
+            padding: theme.spacing(4),
+            marginBottom: theme.spacing(4),
+            borderRadius: 8,
             "& code": {
                 direction: "ltr",
                 display: "inline-block",
@@ -40,31 +54,13 @@ const useStyles = makeStyles((theme: Theme) =>
                 color: "rgb(32, 38, 45)",
                 backgroundColor: "rgba(102, 178, 255, 0.15)",
             },
+            "& >*": {
+                marginTop: theme.spacing(1),
+                marginBottom: theme.spacing(1),
+            },
         },
     })
 );
-
-function ActiveLink({ path }: { path: string }) {
-    const router = useRouter();
-
-    const GoTo = (e: React.MouseEvent<HTMLSpanElement>) => {
-        e.preventDefault();
-        router.push(path);
-    };
-
-    return (
-        <Link
-            href={path}
-            target={"_blank"}
-            rel={"noopener nofollow"}
-            underline={"hover"}
-            onClick={GoTo}
-        >
-            {path}
-        </Link>
-    );
-}
-
 /**
  * @description 首页
  * @return {JSX.Element}
@@ -72,36 +68,19 @@ function ActiveLink({ path }: { path: string }) {
  */
 export default function Home() {
     const styles = useStyles();
+    const [hostname, setHostname] = useState("");
 
-    const table: TableType[] = [
-        {
-            name: "method",
-            effect: "返回值的方法，例如跳转、json",
-            example: "json / plain / element / redirect",
-            default: "redirect",
-        },
-        {
-            name: "idx",
-            effect: "返回图片的日期，在0-8范围内",
-            example: "0（今天） / 1（昨天）",
-            default: "0",
-        },
-        {
-            name: "key",
-            effect: "返回json/element的key",
-            example: "image",
-            default: "url",
-        },
-    ];
-
-    const table1: TableType[] = [
-        {
-            name: "idx",
-            effect: "返回图片的日期，在0-8范围内",
-            example: "0（今天） / 1（昨天）",
-            default: "0",
-        },
-    ];
+    useEffect(() => {
+        setHostname(
+            window.location.port === "80" || window.location.port === "443"
+                ? window.location.protocol + "//" + window.location.hostname
+                : window.location.protocol +
+                      "//" +
+                      window.location.hostname +
+                      ":" +
+                      window.location.port
+        );
+    }, []);
 
     return (
         <>
@@ -110,15 +89,19 @@ export default function Home() {
             </Head>
             <CssBaseline />
             <HeaderBar title={"Home"} />
-            <Container maxWidth={"xl"}>
-                <Box mt={10} mb={4} className={styles.root}>
-                    <Typography variant={"h4"} component={"h1"} mb={2}>
-                        {"# Bing Image API"}
+            <Container maxWidth={"md"} className={styles.container}>
+                <Paper
+                    className={styles.paper}
+                    elevation={3}
+                    variant={"outlined"}
+                >
+                    <Typography variant={"h4"} component={"h1"}>
+                        {"Bing Image API"}
                     </Typography>
-                    <Typography variant={"body2"} component={"p"} mb={1}>
+                    <Typography variant={"body2"} component={"p"}>
                         {"一个Bing每日壁纸的API系统"}
                     </Typography>
-                    <Typography variant={"body2"} component={"p"} mb={1}>
+                    <Typography variant={"body2"} component={"p"}>
                         {"开源代码："}
                         <Link
                             href={"https://github.com/AH-dark/BingImageApi"}
@@ -130,160 +113,20 @@ export default function Home() {
                         </Link>
                     </Typography>
                     <Divider />
-                    <Typography variant={"h5"} component={"h2"} mb={1} mt={1}>
-                        {"## 使用"}
-                    </Typography>
-                    <Typography variant={"h6"} component={"h3"} mb={1} mt={1}>
-                        {"### 每日图片"}
+                    <Typography variant={"h5"} component={"h2"}>
+                        {"每日图片"}
                     </Typography>
                     <Typography variant={"body1"} component={"p"}>
                         {"API地址："}
-                        <code>
-                            {"https://bing-image-api.vercel.app/api/new"}
-                        </code>
+                        <code>{hostname + "/api/new"}</code>
                         <br />
-                        会直接返回302请求到图片，实测可正常使用。 详见
-                        <Link
-                            href={"https://www.alphapic.org.cn"}
-                            target={"_blank"}
-                            rel={"noopener"}
-                        >
-                            www.alphapic.org.cn
-                        </Link>
-                        壁纸
-                    </Typography>
-                    <Typography variant={"h6"} component={"h3"} mb={1} mt={1}>
-                        {"### 近期图片"}
-                    </Typography>
-                    <Typography variant={"body1"} component={"p"}>
-                        {"API地址："}
-                        <code>
-                            {"https://bing-image-api.vercel.app/api/image"}
-                        </code>
-                        <br />
-                        {
-                            "GET请求会直接返回302请求到图片，POST请求会输出bing获取的对应图片的json信息"
-                        }
-                        <TableContainer
-                            component={Paper}
-                            style={{
-                                marginTop: 8,
-                                marginBottom: 12,
-                            }}
-                        >
-                            <Table
-                                sx={{ minWidth: 650 }}
-                                aria-label="simple table"
-                            >
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>查询字符串</TableCell>
-                                        <TableCell>作用</TableCell>
-                                        <TableCell>示例</TableCell>
-                                        <TableCell>默认</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {table1.map((row) => (
-                                        <TableRow
-                                            key={row.name}
-                                            sx={{
-                                                "&:last-child td, &:last-child th":
-                                                    { border: 0 },
-                                            }}
-                                        >
-                                            <TableCell
-                                                component="th"
-                                                scope="row"
-                                            >
-                                                {row.name}
-                                            </TableCell>
-                                            <TableCell>{row.effect}</TableCell>
-                                            <TableCell>{row.example}</TableCell>
-                                            <TableCell>{row.default}</TableCell>
-                                        </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                    </Typography>
-                    <Typography variant={"h6"} component={"h3"} mb={1} mt={1}>
-                        {"### 自定义图片"}
-                    </Typography>
-                    <Typography variant={"body1"} component={"p"} mb={1}>
-                        {"API地址："}
-                        <code>
-                            {"https://bing-image-api.vercel.app/api/custom"}
-                        </code>
-                        <br />
-                        {"参数："}
-                        <TableContainer
-                            component={Paper}
-                            style={{
-                                marginTop: 8,
-                                marginBottom: 12,
-                            }}
-                        >
-                            <Table
-                                sx={{ minWidth: 650 }}
-                                aria-label="simple table"
-                            >
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>查询字符串</TableCell>
-                                        <TableCell>作用</TableCell>
-                                        <TableCell>示例</TableCell>
-                                        <TableCell>默认</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {table.map((row) => (
-                                        <TableRow
-                                            key={row.name}
-                                            sx={{
-                                                "&:last-child td, &:last-child th":
-                                                    { border: 0 },
-                                            }}
-                                        >
-                                            <TableCell
-                                                component="th"
-                                                scope="row"
-                                            >
-                                                {row.name}
-                                            </TableCell>
-                                            <TableCell>{row.effect}</TableCell>
-                                            <TableCell>{row.example}</TableCell>
-                                            <TableCell>{row.default}</TableCell>
-                                        </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                        {"你可以访问尝试一下"}
-                        <ul style={{ margin: "2px 0" }}>
-                            <li>
-                                <ActiveLink path={"/api/custom?method=json"} />
-                            </li>
-                            <li>
-                                <ActiveLink
-                                    path={"/api/custom?method=json&key=image"}
-                                />
-                            </li>
-                            <li>
-                                <ActiveLink path={"/api/custom?method=plain"} />
-                            </li>
-                            <li>
-                                <ActiveLink
-                                    path={"/api/custom?method=element&key=img"}
-                                />
-                            </li>
-                        </ul>
+                        会直接返回今天的壁纸图片
                     </Typography>
                     <Divider />
-                    <Typography variant={"h5"} component={"h2"} mb={1} mt={1}>
-                        {"## CDN"}
+                    <Typography variant={"h5"} component={"h2"}>
+                        {"CDN"}
                     </Typography>
-                    <Typography variant={"body1"} component={"p"} mt={1} mb={1}>
+                    <Typography variant={"body1"} component={"p"}>
                         {"你可以使用AHdark部署的带有CDN的函数"}
                         <br />
                         <Link
@@ -298,8 +141,9 @@ export default function Home() {
                             "此站点使用腾讯云CDN、CloudFront CDN进行边缘网络加速，给予用户更好的体验。"
                         }
                     </Typography>
-                </Box>
+                </Paper>
             </Container>
+            <Box className={styles.background} />
         </>
     );
 }
