eab2c156702701181337cbc1ce1fb0745665647d
AH-dark <ahdark@outlook.com>
2022-03-17 19:11:27 +0800

    Chore: modified type filenames

--- types/config/config.type.ts
+++
@@ -1,3 +0,0 @@
-export type ConfigType = {
-    endpoint: string;
-};
--- types/config/table.config.ts
+++
@@ -1,6 +0,0 @@
-export type TableType = {
-    default: string;
-    effect: string;
-    name: string;
-    example: string;
-};
